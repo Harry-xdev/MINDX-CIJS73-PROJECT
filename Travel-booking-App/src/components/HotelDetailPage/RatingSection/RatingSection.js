b9f8b3a377009be78901015cc687b501874311b0
Dino-Nguyen <xiao.dinnie@gmail.com>
2022-04-25 19:18:59 +0700

    add files

--- Travel-booking-App/src/components/HotelDetailPage/RatingSection/RatingSection.js
+++ Travel-booking-App/src/components/HotelDetailPage/RatingSection/RatingSection.js
@@ -1,2 +1,3 @@
 // render Số Sao đánh giá , số lượt feedback , các cmt / các post phản hồi của khách hàng cho từng khách sạn cụ thể
 // lưu các post feedback trên mock api để người dùng khác có thể xem dc 
+// render box feedback / đánh giá , để người dùng nhập vào.
