05a1e9e2d0a1f8ad5bf3502e5d40cd714692b4bc
Harry-xdev <harry.xdev@gmail.com>
2022-04-25 08:11:03 +0700

    something

--- src/components/HeaderBar/Native-Container/index.js
+++ src/components/HeaderBar/Native-Container/index.js
@@ -2,11 +2,11 @@ import './container.css'
 export const NativeContainer = () => {
     return (
         <div className="native-cont">
-            <li><a href='#'>Flight + Hotel</a></li>
-            <li><a href='#'>Hotel & Home</a></li>
-            <li><a href='#'>Flight</a></li>
-            <li><a href='#'>Today's deals</a></li>
-            <li><a href='#'>Apartments</a></li>
+            <li><a href='https://github.com/Harry-xdev/MINDX-CIJS73-PROJECT'>Flight + Hotel</a></li>
+            <li><a href='https://github.com/Harry-xdev/MINDX-CIJS73-PROJECT'>Hotel & Home</a></li>
+            <li><a href='https://github.com/Harry-xdev/MINDX-CIJS73-PROJECT'>Flight</a></li>
+            <li><a href='https://github.com/Harry-xdev/MINDX-CIJS73-PROJECT'>Today's deals</a></li>
+            <li><a href='https://github.com/Harry-xdev/MINDX-CIJS73-PROJECT'>Apartments</a></li>
         </div>
     )
 }
