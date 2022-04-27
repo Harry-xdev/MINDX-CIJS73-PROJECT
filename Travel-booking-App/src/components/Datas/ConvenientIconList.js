9f62e0ecd68141aaa00af2d75407438fe073f3f6
Dino-Nguyen <xiao.dinnie@gmail.com>
2022-04-28 00:17:03 +0700

    update id icon

--- Travel-booking-App/src/components/Datas/ConvenientIconList.js
+++ Travel-booking-App/src/components/Datas/ConvenientIconList.js
@@ -2,43 +2,53 @@
 
 const ConvenientList = [
     {
+        id : 1,
         name : "Bữa sáng",
         img : "./components/Datas/Images/Convenient-Icon/Bữa sáng.jpg"
     },
     {
+        id : 2,
         name : "Đáng tiền nhất",
         img : "./components/Datas/Images/Convenient-Icon/Đáng tiền nhất.jpg"
     },
     {
+        id : 3,
         name : "Đưa đón sân bay",
         img : "./components/Datas/Images/Convenient-Icon/Đưa đón sân bay.jpg"
     },
     {
+        id : 4,
         name : "Gần phương tiện công cộng",
         img : "./components/Datas/Images/Convenient-Icon/Gần phương tiện công cộng.jpg"
     },
     {
+        id : 5,
         name : "Mới sửa sang",
         img : "./components/Datas/Images/Convenient-Icon/Mới sửa sang.jpg"
     },
     {
+        id : 6,
         name : "Mới xây dựng",
         img : "./components/Datas/Images/Convenient-Icon/Mới sửa sang.jpg"
     },
     {
+        id : 7,
         name : "Nhận phòng 24 giờ",
         img : "./components/Datas/Images/Convenient-Icon/Nhận phòng 24g.jpg"
     },
     {
+        id : 8,
         name : "Wifi miễn phí",
         img : "./components/Datas/Images/Convenient-Icon/Wifi miễn phí.jpg"
     },
     {
-        name : "Dọn phòng hằng ngày",
+        id : 9,
+        name  : "Dọn phòng hằng ngày",
         img : "./components/Datas/Images/Convenient-Icon/Wifi miễn phí.jpg"
     },
     {
-        name : "Bãi biển riêng",
+        id : 10,
+        name  : "Bãi biển riêng",
         img : "./components/Datas/Images/Convenient-Icon/Wifi miễn phí.jpg"
     },
 ]
