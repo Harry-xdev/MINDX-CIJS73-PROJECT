33d7f4b55b984c82bc28bf00471cce40449db49b
Dino-Nguyen <xiao.dinnie@gmail.com>
2022-04-29 03:46:01 +0700

    suggest

--- Travel-booking-App/src/components/Pages/HomePage/Suggestion/index.js
+++ Travel-booking-App/src/components/Pages/HomePage/Suggestion/index.js
@@ -2,11 +2,14 @@ import './suggestion.css'
 import axios from 'axios'
 export const Suggestion = () => {
 
-    const handleFetchHotelList = []
+    const handleFetchHotelList = async ({suggest}) => {
+        const response = await fetch(`https://6268162901dab900f1c9969b.mockapi.io/hotelList/:${suggest}`)
+        const data = await response.json();
+    }
 
     return (
     <div className="suggestion">
-            
+            suggestion
         </div>
     )
 }
