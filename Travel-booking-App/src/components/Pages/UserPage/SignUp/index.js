70314b8e6b53dac1a7f5df74702d5a6f412a076a
Harry-xdev <harry.xdev@gmail.com>
2022-05-06 10:33:11 +0700

    login

--- Travel-booking-App/src/components/Pages/UserPage/SignUp/index.js
+++ Travel-booking-App/src/components/Pages/UserPage/SignUp/index.js
@@ -2,18 +2,21 @@ import { useEffect, useState } from 'react'
 import './signup.css'
 
 export const SignUp = () => {
-
+    
     const [accounts, setAccounts] = useState([])
     const [firstName, setFirstName] = useState("")
     const [lastName, setLastName] = useState("")
-    const [email, setEmail] = useState([""])
+    const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-    const [repassword, setRepassword] = useState("")
+    const [cfmpassword, setCfmpassword] = useState("")
+
+    
 
     const fecthData = async () => {
         const response = await fetch('https://6268162901dab900f1c9969b.mockapi.io/appi/v1/userList')
         const accData = await response.json()
         setAccounts(accData)
+        
     }
 
     useEffect(() => {
@@ -21,17 +24,19 @@ export const SignUp = () => {
     }, [])
     console.log(accounts)
 
-    // function submit() {
-    //     console.warn(firstName, lastName, email, password, repassword)
+    // let arrEmail = accounts.filter(emailList)
+    // const emailList = () => {
+    //     email
     // }
+    // console.log(arrEmail)
 
-    const addNewAccount = ({firstName, lastName, email, password}) => {
-        const res = fetch('https://6268162901dab900f1c9969b.mockapi.io/appi/v1/userList', {
+    const addNewAccount = ({firstName, lastName, email, password, cfmpassword}) => {
+        fetch('https://6268162901dab900f1c9969b.mockapi.io/appi/v1/userList', {
             method: 'POST',
-            headers : {
+            headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({firstName, lastName, email, password}),
+            body: JSON.stringify({firstName, lastName, email, password, cfmpassword}),
         })
     }
 
@@ -42,37 +47,56 @@ export const SignUp = () => {
                 <label>First Name</label>
                 <input placeholder='Your first name'
                     type='text'
+                    value={firstName}
                     onChange={(e) => setFirstName(e.target.value)}
                 />
 
                 <label>Last Name</label>
                 <input placeholder='Your last name'
                     type='text'
+                    value={lastName}
                     onChange={(e) => setLastName(e.target.value)}
                 />
 
                 <label>Email</label>
                 <input placeholder='Email'
                     type='email'
+                    value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
 
                 <label>Password</label>
                 <input placeholder='Password'
                     type='password'
+                    value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
                 <label>Comfirm password</label>
                 <input placeholder='Comfirm password'
                     type='password'
-                    onChange={(e) => setRepassword(e.target.value)}
+                    value={cfmpassword}
+                    onChange={(e) => setCfmpassword(e.target.value)}
                 />
             </form>
             <div>
                 <input type='checkbox' id='checkbox' name='checkbox' />
                 <label htmlFor='checkbox'>Remmber me</label>
             </div>
-            <button onClick={addNewAccount} >Sign Up</button>
+            <button
+                onClick={() => {
+                    
+                    addNewAccount({
+                        firstName: firstName,
+                        lastName: lastName,
+                        email : email,
+                        password : password,
+                        cfmpassword: cfmpassword,
+                    })
+                    
+
+                    alert(`You have signed up!`)
+                }}>Sign Up
+            </button>
             <button>Already have an account? Sign in</button>
             <div><p>By signing in, I angree to Agoda's <a href='https://www.google.com'>Term of Use</a> and <a href='https://www.google.com'>Privacy Policy.</a></p></div>
 
